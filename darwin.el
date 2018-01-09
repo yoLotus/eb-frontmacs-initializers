af35bda93549afa5f84e30fe44eee5745841b913
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-01-09 14:26:39 +0100

    update darwin.el file

--- darwin.el
+++ darwin.el
@@ -1,10 +1,17 @@
-;;; darwin.el
-;;; handle MAX OS X keyboard to move the meta key to left cmd
+;;; darwin.el -- make some actions on start up if OS X is detected
+
+;;; Commentary:
+;; nil now
+
+;;; Code:
 
 (if (eq system-type 'darwin)
     (progn
+      ;; handle MAX OS X keyboard to move the meta key to left cmd
       (setq mac-command-modifier 'meta)
       (setq mac-right-option-modifier 'none)
+
+      ;; I use an AZERTY keyboard (oups)
       (global-set-key (kbd "M-n") '(lambda () (interactive) (insert "~")))))
 
-;; darwin.el ends here
+;;; darwin.el ends here

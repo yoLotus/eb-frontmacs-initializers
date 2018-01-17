df622ca8204944cc2337e930ef8038a4a0930269
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-01-17 11:51:59 +0100

    add js initializer

--- custom-variables.el
+++ custom-variables.el
@@ -8,8 +8,5 @@
 ;; grep
 (setq grep-command "grep -nrIH --exclude-dir=\".git\" --exclude-dir=\"tmp\" --exclude-dir=\"log\" --exclude-dir=\"node_modules\" ")
 
-;; js2
-(setq js2-strict-missing-semi-warning nil)
-
 (provide 'custom-variables)
 ;;; custom-variables.el ends here
---
+++ eb-js-mode.el
@@ -0,0 +1,13 @@
+;;; eb-js-mode.el -- personal customizations for js mode
+
+;;; Commentary:
+;; nil for now
+
+;;; Code:
+
+;; remove semi-column js2 warning
+(require 'js2-mode)
+(setq js2-strict-missing-semi-warning nil)
+
+(provide 'misc)
+;;; eb-js-mode.el ends here

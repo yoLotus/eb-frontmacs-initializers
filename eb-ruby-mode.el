0f9bf6cc3225f52a738432fe4e75863d6fe7cb4a
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-04-24 17:36:40 +0200

    add file for ruby mode

---
+++ eb-ruby-mode.el
@@ -0,0 +1,13 @@
+;;; eb-ruby-mode.el -- personal customizations for ruby mode
+
+;;; Commentary:
+;; nil for now
+
+;;; Code:
+
+(require 'ruby-mode)
+(setq require-final-newline t)
+(setq ruby-insert-encoding-magic-comment nil)
+
+(provide 'eb-ruby-mode)
+;;; eb-ruby-mode.el ends here

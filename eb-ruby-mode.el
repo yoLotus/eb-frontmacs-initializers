f7a54eb66679161bc3fc39adaac309243e37362a
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-06-15 16:01:21 +0200

    active ruby tools for haml mode

--- eb-ruby-mode.el
+++ eb-ruby-mode.el
@@ -9,5 +9,8 @@
 (setq require-final-newline t)
 (setq ruby-insert-encoding-magic-comment nil)
 
+;; enable ruby-tools-mode for haml file
+(add-hook 'haml-mode-hook 'ruby-tools-mode)
+
 (provide 'eb-ruby-mode)
 ;;; eb-ruby-mode.el ends here

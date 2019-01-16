9b9563a8ddee014eb2f762d38089be3560a8b299
Etienne Bazin <etienne@talent.io>
2019-01-16 16:15:45 +0100

    disable prettier

--- eb-js-mode.el
+++ eb-js-mode.el
@@ -14,10 +14,10 @@
 (setq js-switch-indent-offset 2)
 
 ;; prettier
-(setq-default prettier-js-args '("--no-semi" "--single-quote"))
+;; (setq-default prettier-js-args '("--no-semi" "--single-quote"))
 ;;; enable it on all jsx? files
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'web-mode-hook 'prettier-js-mode)
+;; (add-hook 'js2-mode-hook 'prettier-js-mode)
+;; (add-hook 'web-mode-hook 'prettier-js-mode)
 
 ;;; rjsx-mode
 

5ddff187f6fa5edd10cdec4cc4f2e35c52cab771
Etienne Bazin <etienne@talent.io>
2019-02-15 11:18:04 +0100

    remove annoying alarm bell

--- misc.el
+++ misc.el
@@ -28,5 +28,7 @@
 (global-unset-key (kbd "M-<down-mouse-1>"))
 (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
 
+;; remove annoying alarm
+(setq ring-bell-function 'ignore)
 (provide 'misc)
 ;;; misc.el ends here

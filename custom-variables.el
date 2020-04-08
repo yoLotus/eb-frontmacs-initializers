0eca9e02d7585a4f8b67b4f6241275f47a88b1e6
yoLotus <bazin.etienne@yahoo.fr>
2020-04-08 12:15:28 +0200

    magit auto save buffer in repo when opening maggit buffer

--- custom-variables.el
+++ custom-variables.el
@@ -8,5 +8,10 @@
 ;; grep
 (setq grep-command "grep -nrIH --exclude-dir=\".git\" --exclude-dir=\"tmp\" --exclude-dir=\"log\" --exclude-dir=\"node_modules\" ")
 
+;; magit
+(require 'magit)
+; do not ask to save buffer before open magit buffers
+(setq magit-save-repository-buffers 'dontask)
+
 (provide 'custom-variables)
 ;;; custom-variables.el ends here
